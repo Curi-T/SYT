bdc5a9370891b2a3170ebe5b8efea9f91829cfdd
curit <curit@qq.com>
2022-07-10 16:09:02 +0800

    24 医院设置controller，日志

---
+++ common/common_util/src/main/java/cn/cqut/yygh/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,33 @@
+package cn.cqut.yygh.common.exception;
+
+import cn.cqut.yygh.common.result.Result;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author CuriT
+ * @Date 2022-7-10 15:49
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public Result error(Exception e){
+        e.printStackTrace();
+        return Result.fail();
+    }
+
+    /**
+     * 自定义异常处理方法
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(YyghException.class)
+    @ResponseBody
+    public Result error(YyghException e){
+        return Result.build(e.getCode(), e.getMessage());
+    }
+}
+
--- service/service_hosp/src/main/java/cn/cqut/yygh/hosp/ServiceHospApplication.java
+++ service/service_hosp/src/main/java/cn/cqut/yygh/hosp/ServiceHospApplication.java
@@ -2,12 +2,14 @@ package cn.cqut.yygh.hosp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author CuriT
  * @Date 2022-7-8 17:05
  */
 @SpringBootApplication
+@ComponentScan(basePackages = "cn.cqut")
 public class ServiceHospApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceHospApplication.class, args);
