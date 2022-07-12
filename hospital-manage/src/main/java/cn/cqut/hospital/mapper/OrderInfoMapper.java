c573ee6accb1edcc19ec74a633589ac2d6b16f82
curit <curit@qq.com>
2022-07-12 11:25:11 +0800

    74 整合 数据接口-医院上传接口

---
+++ hospital-manage/src/main/java/cn/cqut/hospital/mapper/OrderInfoMapper.java
@@ -0,0 +1,10 @@
+package cn.cqut.hospital.mapper;
+
+import cn.cqut.hospital.model.OrderInfo;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface OrderInfoMapper extends BaseMapper<OrderInfo> {
+
+}
